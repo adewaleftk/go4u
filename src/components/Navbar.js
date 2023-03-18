9ba5e0faaaea696f1baf6a1d5788f09a32b83736
Adewale Fatoki <adewaleftk@gmail.com>
2023-03-18 17:27:42 +0100

    Testing go4u

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,4 @@
-import React, { useState } from 'react';
-// import { useRef } from 'react';
-// import { FaBars, FaTimes } from "react-icons/fa"
+import React from 'react';
 import "../styles/navbar.css";
 
 function Navbar() {
